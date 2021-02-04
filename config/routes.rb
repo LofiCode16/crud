11ea4be307064aac52397d2b9bffc20b3d09793c
Diego Perez <diegopr178@gmail.com>
2021-02-04 17:03:46 -0300

    crud with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # resources :posts, only: [:index, :show, :destroy]
+  # resources :posts, except: [:index, :show], path: 'textos'
+
+  resources :posts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
 end
