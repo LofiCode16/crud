00670ba9ad46b4a7f847b68083ae963370ad5b66
Diego Perez <diegopr178@gmail.com>
2021-02-05 12:51:52 -0300

    test with minitest

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup 
+    @post = Post.new(
+                      title: 'post uno', 
+                      content: 'contenido con espacios, letras y numeros', 
+                      author: 'diego'
+                      )
+  end
+
+  test 'post is valid' do
+    assert @post.valid?
+  end
+
+  test 'post is not valid without a title' do
+    @post.title = nil
+
+    refute @post.valid?, 'El post está siendo válido sin un título'
+  end
+
+  test 'title is valid only with letters' do
+    @post.title = 123
+
+    refute @post.valid?, 'el titulo no puede contener numeros'
+  end
 end
