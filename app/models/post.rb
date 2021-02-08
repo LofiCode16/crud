0359283e9f0236603f7943f208e3b2cca4de9395
Diego Perez <diegopr178@gmail.com>
2021-02-08 16:40:38 -0300

    add scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
-    validates :title, presence: true
+    # validates :title, presence: true
+    # validates_presence_of :title
     # validates :title, presence: :true, length: { minimum: 5, maximum: 20 }
     # validates :content, presence: :true, length: { minimum: 10, maximum: 300 }
     # validates :author, presence: :true, length: { minimum:3 }
-    # validates :title, format: { with: /\A[a-zA-Z\s]+\z/, message: 'Sólo puede contener letras' }  
+    validates :title, format: { with: /\A[a-zA-Z\s]+\z/, message: 'Sólo puede contener letras' }  
     # validates :author, inclusion: { in: ['diego', 'david', 'ricardo'], message: 'No eres un autor válido' }
-    # # validates :author, numericality: { only_integer: true }
+    # validates :author, numericality: { only_integer: true }
     # before_save :verificar_dv
 
     # enum author: [:diego, :david, :ricardo]
@@ -14,5 +15,11 @@ class Post < ApplicationRecord
     # def verificar_dv
     #     # hacer algo con el registro antes de guardarlo usando el callback before_save
     # end
-end
 
+    scope :post_of_today, -> { where('created_at LIKE ?', "%#{Time.now.to_s[0..9]}%") }
+    scope :find_post_by_title, -> (title){ where('title LIKE ?', "%#{title}%") }
+
+    # def self.post_of_today
+    #     where('created_at LIKE ?', "%#{Time.now.to_s[0..9]}%")
+    # end
+end
